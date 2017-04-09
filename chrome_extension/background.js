a4e1ced55a0262ceac98c073a4056acea880ae57
Alexander Jia <jayjialex@gmail.com>
2017-04-09 04:45:53 -0400

    removed generated background.html
    now properly looks at the current DOM file (the calendar.google.com)

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,22 +1,33 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) 
-{
-	console.log("Hello");
-	if (changeInfo.status == 'complete') //if tab is not fully loaded, ignore.
-	{
-	console.log("Hullo");
-		var match = 'https://calendar.google.com/calendar/render';
-		var id = "fkndgnmacepohnggjbhgiajoaggjmgim";
-		if (tab.url.substring(0, match.length) === match)
-		{
-			console.log("Herro");
-			//chrome.tabs.executeScript(tabId, { file: 'weather.js' }); //if URL match is found, run said script.
-		//}
-		
+//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) 
+//{
+window.onload = function(event) {
 			var locations = document.getElementsByClassName('neb-break-words');
 			var timeblock = document.getElementsByClassName('neb-date');
 			var eventName = document.getElementsByClassName('ui-sch');
+			
+			console.log(locations);
+			console.log(timeblock);
+			console.log(eventName);
+			document.body.innerHTML+='<div>Pizza</div>';
+		};
+	
+// }	console.log("Hello");
+	// if (changeInfo.status == 'complete') //if tab is not fully loaded, ignore.
+	// {
+	// console.log("Hullo");
+		// var match = 'https://calendar.google.com/calendar/render';
+		// var id = "fkndgnmacepohnggjbhgiajoaggjmgim";
+		// if (tab.url.substring(0, match.length) === match)
+		// {
+			// console.log("Herro");
+	//		chrome.tabs.executeScript(tabId, { file: 'weather.js' }); //if URL match is found, run said script.
+		//}
+		
+			// var locations = document.getElementsByClassName('neb-break-words');
+			// var timeblock = document.getElementsByClassName('neb-date');
+			// var eventName = document.getElementsByClassName('ui-sch');
 			//console.log(locations);
 			
-		}
-	}
-});
+		// }
+	// }
+//});
