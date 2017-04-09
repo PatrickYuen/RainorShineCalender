dc42e6472e8a6a57565f6d92f60a518289240f33
Alexander Jia <jayjialex@gmail.com>
2017-04-09 04:25:49 -0400

    init commit for chrome extension

---
+++ chrome_extension/weather.js
@@ -0,0 +1,16 @@
+(function() {
+
+var id = "fkndgnmacepohnggjbhgiajoaggjmgim"; //our chrome extension id
+
+var testImg = new Image();
+// testImg.src = 'chrome-extension://'+ id +'icon.png';
+// testImg.style.position = 'absolute';
+// testImg.style.display = 'none;
+
+var locations = document.getElementsByClassName('neb-break-words');
+var timeblock = document.getElementsByClassName('neb-date');
+var eventName = document.getElementsByClassName('ui-sch');
+	console.log("heh");
+	//POST to server.
+		//POST to weather API.
+})();
